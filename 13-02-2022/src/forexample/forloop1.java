fef57710bf99f84d474feb141824e4cb325b21da
Acer <Acer@DESKTOP-HIP07M8>
2022-02-14 00:03:08 +0530

    first commmit

---
+++ 13-02-2022/src/forexample/forloop1.java
@@ -0,0 +1,17 @@
+package forexample;
+
+public class forloop1 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int i,n;
+		for(i=0;i<=5;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
